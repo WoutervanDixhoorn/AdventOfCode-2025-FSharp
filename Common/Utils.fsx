b9148d2dcd1d9668c8de9ca2d9ce630b5e7123f5
Wouter van Dixhoorn <woutervdixhoorn@gmail.com>
2025-12-08 16:05:24 +0100

    Day 6 p2 | I finally managed to solve part 2, I needed to remove the Trim() from my util, this was hellish :clown_face:

--- Common/Utils.fsx
+++ Common/Utils.fsx
@@ -5,14 +5,14 @@ open System
 let readInput () =
     Seq.initInfinite (fun _ -> Console.ReadLine())
         |> Seq.takeWhile (fun line -> line <> null)
-        |> Seq.map (fun line -> line.Replace("\uFEFF", "").Trim())
+        |> Seq.map (fun line -> line.Replace("\uFEFF", ""))
         |> Seq.filter(fun line -> not (String.IsNullOrEmpty(line)))
         |> Seq.toList
 
 let readInputAsOneLine () =
     Seq.initInfinite (fun _ -> Console.ReadLine())
         |> Seq.takeWhile (fun line -> line <> null)
-        |> Seq.map (fun line -> line.Replace("\uFEFF", "").Trim())
+        |> Seq.map (fun line -> line.Replace("\uFEFF", ""))
         |> Seq.filter (fun line -> not (String.IsNullOrEmpty(line)))
         |> String.concat ""
 
--- Day 6/main.fsx
+++ Day 6/main.fsx
@@ -6,10 +6,11 @@ open System
 //Run using: Get-Content input.txt | dotnet fsi main.fsx
 let input = readInput()
 
-let parsedInput = 
+let parsedInputOne = 
     input |> 
     List.map (fun line -> 
-        line.Split([|' '|], System.StringSplitOptions.RemoveEmptyEntries) |> Array.toList
+        line.Split([|' '|], System.StringSplitOptions.RemoveEmptyEntries) |>
+        Array.toList
     )
 
 let useOp op a b=
@@ -19,18 +20,18 @@ let useOp op a b=
     | _ -> failwith "Unknown operator"
 
 let opsStack = 
-    parsedInput |>
+    parsedInputOne |>
     List.filter (fun line -> 
         line |> List.forall (fun str ->
             match Int64.TryParse str with
             | (true, value) -> false
-            | (false, _)            -> true
+            | (false, _)    -> true
         )
     ) |>
     List.collect id
 
 let numberLines = 
-    parsedInput |>
+    parsedInputOne |>
     List.filter (fun line -> 
         line |> List.forall (fun str ->
             match Int64.TryParse str with
@@ -53,34 +54,6 @@ let rec transpose charList =
             let heads    = List.map List.head rows 
             let tails = List.map List.tail rows
             heads :: transpose tails
-
-let padAndTranspose (stackLines: string list) =
-    let maxWidth = stackLines |> List.map String.length |> List.max
-    stackLines |> 
-    List.map (fun s -> s.PadLeft(maxWidth, ' ')) |> 
-    List.map Seq.toList |> 
-    transpose |> 
-    List.map (fun chars -> 
-        chars |> 
-        List.filter (fun c -> c <> ' ') |> 
-        String.Concat
-    )
-
-let numberStacksTwo (stackLines) =
-    let maxLineLength = stackLines |> List.map String.length |> List.max
-
-    stackLines |>
-    List.map (fun s -> s.PadRight(maxLineLength, ' ')) |>
-    List.map Seq.toList
-
-
-    // parsedInput |>
-    // List.map (fun stackLine ->
-    //     padAndTranspose stackLine
-    // ) |>
-    // List.map (fun subList ->
-    //     subList |> List.map System.Int64.Parse
-    // )
     
 let partOne = 
     numberStack |>
@@ -93,15 +66,57 @@ let partOne =
     ) |>
     List.sum
 
-// let partTwo =
-//     numberStacksTwo |>
-//     List.mapi (fun i numsLine ->
-//         let op = opsStack.[i]
-//         numsLine |> 
-//         List.reduce (fun a b -> 
-//             useOp op a b
-//         )    
-//     ) |>
-//     List.sum
+printf "PartOne: %A\n" partOne
 
-printf "%A\n" partOne
+//Part two 
+//1. Read input
+//2. Chunk input into problem blocks
+//3. transpose
+//4. reduce
+
+let normalizeGrid (lines: string list) =
+    let maxLen = lines |> List.map (fun s -> s.Length) |> List.max
+    lines |> List.map (fun s -> s.PadRight(maxLen, ' '))
+
+let paddedInput = normalizeGrid input
+let columns = 
+    paddedInput
+    |> List.take (paddedInput.Length - 1) 
+    |> List.map Seq.toList 
+    |> transpose
+
+let isSpace col = 
+    col |> List.forall (fun c -> c = ' ')
+
+let problemBlocksChars =
+    let (lastBlock, collectedBlocks) = 
+        columns |>
+        List.fold (fun (currentBlock, allBlocks) col ->
+            if isSpace col then 
+                ([], currentBlock :: allBlocks)
+            else
+                (col :: currentBlock, allBlocks)
+        ) ([],[]) 
+    (lastBlock :: collectedBlocks)
+
+let problemBlocksNums = 
+    problemBlocksChars |>
+    List.map (fun blocks ->
+        blocks |>
+        List.map Seq.toList |>
+        List.map String.Concat |>
+        List.map int64  
+    )
+
+let partTwo =
+    problemBlocksNums |>
+    List.mapi (fun i block ->
+        let opReversed = opsStack |> List.rev
+        block |>
+        List.reduce (fun a b -> 
+            useOp opReversed.[i] a b
+        ) 
+    ) |> 
+    List.sum
+
+printf "PartTwo: %A\n" partTwo
